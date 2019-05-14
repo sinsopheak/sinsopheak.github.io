793be3283661b168a0c8f0df1d2637d4aed8040d
Sopheak Sin <sin.sopheak@outlook.com>
2019-05-14 16:45:31 +0700

    change jquery to javascript

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     $('.header').height($(window).height());
 })
 
+var windowh = window.innerHeight;
+document.getElementsByClassName('header').style.height = windowh;
+
 let myTypeItInstance = new TypeIt('#simpleUsage', {
     speed: 60,
     startDelay: 900
