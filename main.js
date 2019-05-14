b23be5c3038df9fe7f027a9844a165bc708493d9
sinsopheak <sin.sopheak@outlook.com>
2019-05-14 18:58:14 +0700

    fixed animation

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ $(document).ready(function () {
     $('.header').height($(window).height());
 })
 
-var windowh = window.innerHeight;
-document.getElementsByClassName('header').style.height = windowh;
-
 let myTypeItInstance = new TypeIt('#simpleUsage', {
     speed: 60,
     startDelay: 900
 })
     .type('Welcome to my portfolio.')
     .go();
+
+
+var windowh = window.innerHeight;
+document.getElementsByClassName('header').style.height = windowh;
+
