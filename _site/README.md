bed66734dd85c8370d17953bb2b9bd326dc2c1c2
sinsopheak <sin.sopheak@outlook.com>
2019-05-18 19:03:05 +0700

    migrated to Jekyll

---
+++ _site/README.md
@@ -0,0 +1,1 @@
+# sinsopheak.github.io
---
+++ _site/assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.header').height($(window).height());
+})
+
+let myTypeItInstance = new TypeIt('#simpleUsage', {
+    speed: 60,
+    startDelay: 900
+})
+    .type('Welcome to my portfolio.')
+    .go();
+
+
+var windowh = window.innerHeight;
+document.getElementsByClassName('header').style.height = windowh;
+
